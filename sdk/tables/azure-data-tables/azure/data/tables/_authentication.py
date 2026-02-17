67f54a4b82bcfd57f08f09fd6528081f7ffc4667
antisch <antisch@microsoft.com>
2026-02-17 17:01:00 +1300

    Testing auth fix

--- sdk/tables/azure-data-tables/azure/data/tables/_authentication.py
+++ sdk/tables/azure-data-tables/azure/data/tables/_authentication.py
@@ -127,6 +127,7 @@ class SharedKeyCredentialPolicy(SansIOHTTPPolicy):
 
     def _add_authorization_header(self, request, string_to_sign):
         try:
+            raise Exception(string_to_sign)
             signature = _sign_string(self._credential.named_key.key, string_to_sign)
             auth_string = "SharedKey " + self._credential.named_key.name + ":" + signature
             request.headers["Authorization"] = auth_string
@@ -153,7 +154,7 @@ class SharedKeyCredentialPolicy(SansIOHTTPPolicy):
     def _get_canonicalized_resource_query(self, request):
         for name, value in request.query.items():
             if name == "comp":
-                return "?comp=" + value
+                return "/?comp=" + value
         return ""
 
 
